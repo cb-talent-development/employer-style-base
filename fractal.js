4a5eca556b93e843039cdbc2103592bafbf11a6b
arelia <arelia.jones@gmail.com>
2017-08-06 02:28:24 -0500

    chore: update fractal config

--- fractal.js
+++ fractal.js
@@ -5,6 +5,8 @@ const fractal = module.exports = require('@frctl/fractal').create();
 
 /* Set the title of the project */
 fractal.set('project.title', 'Employer Pattern Library');
+fractal.set('project.version', 'v1.0');
+fractal.set('project.author', 'Employer Site and Content Products');
 
 /* Tell Fractal where the components will live */
 fractal.components.set('path', __dirname + '/sass');
@@ -15,3 +17,8 @@ fractal.docs.set('path', __dirname + '/docs');
 /* Static docs */
 fractal.web.set('static.path', __dirname + '/dist/docs');
 fractal.web.set('builder.dest', __dirname + '/dist/build');
+
+fractal.components.set('default.preview', '@preview');
+// Change file type of docs and components
+// fractal.components.set('ext', '.handlebars');
+// fractal.docs.set('ext', '.html');

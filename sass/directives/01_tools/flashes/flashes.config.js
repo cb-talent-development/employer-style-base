da4bfc358c22b6506507edf1b4ad4f05fa92a1f5
arelia <arelia.jones@gmail.com>
2017-08-09 22:34:30 -0500

    chore: make quotation marks consistent in config files

--- sass/directives/01_tools/flashes/flashes.config.js
+++ sass/directives/01_tools/flashes/flashes.config.js
@@ -1,41 +1,41 @@
 module.exports = {
-  "collated": true,
+  'collated': true,
   collator: function(markup, item) {
       return `<!-- Start: @${item.handle} -->\n${markup}<br><br>\n<!-- End: @${item.handle} -->\n`
     },
-  "default": "primary",
-  // "status": prototype,
-  "variants": [
+  'default': 'primary',
+  // 'status': prototype,
+  'variants': [
     {
-      "name": "primary",
-      "context": {
-        "order": 1,
-        "modifier": "primary",
-        "label": "primary"
+      'name': 'primary',
+      'context': {
+        'order': 1,
+        'modifier': 'primary',
+        'label': 'primary'
       }
     },
     {
-      "name": "success",
-      "context": {
-        "order": 1,
-        "modifier": "success",
-        "label": "success"
+      'name': 'success',
+      'context': {
+        'order': 1,
+        'modifier': 'success',
+        'label': 'success'
       }
     },
     {
-      "name": "warning",
-      "context": {
-        "order": 1,
-        "modifier": "warning",
-        "label": "warning"
+      'name': 'warning',
+      'context': {
+        'order': 1,
+        'modifier': 'warning',
+        'label': 'warning'
       }
     },
     {
-      "name": "danger",
-      "context": {
-        "order": 1,
-        "modifier": "danger",
-        "label": "danger"
+      'name': 'danger',
+      'context': {
+        'order': 1,
+        'modifier': 'danger',
+        'label': 'danger'
       }
     }
   ]
